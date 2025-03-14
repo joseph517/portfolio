70838a21f30ef2fa55780fa55d81a9cca3879545
alvaro <alvarovergara1993@hotmail.com>
2025-03-13 23:38:43 -0500

    component about has been added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 
 import './App.css'
+import About from './components/about/about'
 import Header from './components/header/header'
+import Home from './components/home/home'
 
 function App() {
 
   return (
     <>
-      <Header />      
+      <Header />
+      <Home />
+      <About />
     </>
   )
 }
